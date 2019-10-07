bbc0d9514130998f3f8124e8df49c35152836a4f
kanitnilnaem <kn22@students.uwf.edu>
2019-10-07 18:41:02 -0500

    testing

--- app/src/main/java/com/example/smartcalendar/MainActivity.java
+++ app/src/main/java/com/example/smartcalendar/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        System.out.println("testing");
+        System.out.println("testing123");
+
     }
 }
